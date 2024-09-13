d48efeabd99d03cb9225e11f5209545c076f96b8
Duc Duy Pham <ddp4@sfu.ca>
2024-09-13 16:53:44 -0700

    Hooked up the database onto backend with Mongoose

--- server/index.js
+++ server/index.js
@@ -1,15 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const mongoUri = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('MongoDB connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Starting the express server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
