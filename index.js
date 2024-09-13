3b045a297f476fbc17caa481f0b91824c4f2fe91
Duc Duy Pham <ddp4@sfu.ca>
2024-09-13 16:24:23 -0700

    Added server side environment, Express, CORS, Mongoose

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
