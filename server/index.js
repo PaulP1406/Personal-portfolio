ddcb5f5a07dfa2f14c0942ce87790afb48de9b5b
PaulP1406 <ducduy0308@gmail.com>
2024-09-17 18:51:12 -0700

    Setted up server side API for sending email with NodeMailer

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const cors = require('cors');
+
 const mongoose = require('mongoose')
+
+const Nodemailer = require('nodemailer');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const mongoUri = process.env.MONGO_URI;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(mongoUri)
@@ -24,6 +29,38 @@ mongoose.connect(mongoUri)
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
+// Nodemailer setup:
+app.post('/api/email', (req, res) => {
+  const { name, email, message } = req.body;
+  
+  const transporter = Nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: `Message from ${name}`,
+    text: message
+  };
+
+  try {
+    transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Email sent' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error sending email' });
+  }
+});
+
 
 // Starting the express server
 app.listen(port, () => {
