3235d815d4b24c58191cbed858ca73257dfefd0b
PaulP1406 <ducduy0308@gmail.com>
2024-09-18 15:06:55 -0700

    Design done for contact page, large screen only

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center pt-16 bg-gray-100 dark:bg-gray-900">
-            <div className="container mx-auto px-6 py-12 lg:flex lg:space-x-12">
+        <div className="min-h-screen flex items-center justify-center pt-16 bg-white dark:bg-gray-900">
+            <div className="container mx-auto px-6 py-12 grid grid-cols-1 lg:grid-cols-8 gap-12">
                 {/* Left Section: Contact Form */}
-                <div className="p-8 shadow-lg rounded-lg w-full lg:w-2/3 bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
-                    <h2 className="text-3xl font-semibold mb-2 text-indigo-600 dark:text-pink-400">Just say Hello!</h2>
-                    <p className="mb-8 text-gray-600 dark:text-gray-300">Let us know more about you!</p>
+                <div className="w-full text-gray-900 dark:text-white col-start-2 col-span-3">
+                    <h2 className="text-3xl font-bold text-gray-700 dark:text-gray-100 text-center lg:text-left">
+                        Just say
+                        <span className="text-5xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+                            &nbsp;Hello!
+                        </span>
+                    </h2>
+                    <p className="mb-8 text-gray-600 dark:text-gray-300">Let's connect! Coffee's on me (and 🥐 too!)</p>
 
                     <form className="space-y-4">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -38,10 +43,10 @@ const Contact = () => {
                             placeholder="Your Message"
                             className="p-4 rounded-lg border bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white border-gray-300 dark:border-gray-600 focus:ring-indigo-600 dark:focus:ring-pink-400 w-full h-32"
                         ></textarea>
-                        <div className="text-center lg:text-right">
+                        <div className="text-center lg:text-left">
                             <button
                                 type="submit"
-                                className="px-6 py-3 rounded-lg shadow-md transition bg-indigo-600 dark:bg-pink-400 text-white hover:bg-indigo-700 dark:hover:bg-pink-500"
+                                className="px-6 py-3 rounded-lg shadow-md transition transform hover:-translate-y-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white hover:bg-indigo-700 dark:hover:bg-pink-500"
                             >
                                 Send Message
                             </button>
@@ -50,19 +55,24 @@ const Contact = () => {
                 </div>
 
                 {/* Right Section: Contact Information */}
-                <div className="mt-8 lg:mt-0 lg:w-1/3 p-8 rounded-lg shadow-lg bg-white dark:bg-blue-900 text-gray-900 dark:text-white">
-                    <h2 className="font-semibold text-xl mb-4 text-indigo-600 dark:text-pink-400">Contact Information</h2>
+                <div className="w-full text-gray-900 dark:text-white col-start-6 col-span-3">
+                    <h2 className="text-3xl font-bold text-gray-700 dark:text-gray-100 text-center mb-8 lg:text-left">
+                        Contact <br /> Information                        
+                    </h2>
                     <p className="text-gray-600 dark:text-gray-300">
-                        77 Baker Street<br />Bondowoso, 87655, Indonesia
+                        <strong>Email:</strong> ducduy0308@gmail.com
                     </p>
-                    <p className="mt-4 text-gray-600 dark:text-gray-300">Call us: +62 81 334 61 00</p>
-                    <p className="mt-4 text-gray-600 dark:text-gray-300">We are open from Monday - Friday<br />08:00 am - 05:00 pm</p>
+                    <p className="mt-4 text-gray-600 dark:text-gray-300"><strong>Mobile:</strong> (+1) 778-710-6344</p>
+                    <p className="mt-4 text-gray-600 dark:text-gray-300"><strong>Location:</strong> Vancouver, BC, Canada</p>
 
-                    <h3 className="font-semibold text-xl mt-8 text-indigo-600 dark:text-pink-400">Follow Us</h3>
+                    <h3 className="mt-12 text-3xl font-bold text-gray-700 dark:text-gray-100 text-center lg:text-left mt-8">
+                        Follow Me
+                        
+                    </h3>
                     <div className="flex space-x-4 mt-4">
-                        <a href="#" className="transition hover:text-indigo-400 dark:hover:text-pink-300">Facebook</a>
-                        <a href="#" className="transition hover:text-indigo-400 dark:hover:text-pink-300">Instagram</a>
-                        <a href="#" className="transition hover:text-indigo-400 dark:hover:text-pink-300">Vimeo</a>
+                        <a href="https://www.linkedin.com/in/ddpham/" className="transition hover:text-indigo-400 dark:hover:text-pink-300">LinkedIn</a>
+                        <a href="https://github.com/PaulP1406" className="transition hover:text-indigo-400 dark:hover:text-pink-300">Github</a>
+                        <a href="#" className="transition hover:text-indigo-400 dark:hover:text-pink-300">Discord</a>
                     </div>
                 </div>
             </div>
