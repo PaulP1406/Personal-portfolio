0b7f33c80deab89deb93bcfa039a5b8ffb0f0169
PaulP1406 <ducduy0308@gmail.com>
2024-09-22 13:49:07 -0700

    Nav bar styling sweep done

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
         
         {/* Left Section: Logo */}
         <div className="flex items-center">          
-          <a href="/" className="ml-20">
+          <a href="/" className="ml-8 lg:ml-20">
             <img src={blackLogo} alt="Logo" className="h-8 lg:h-12 block dark:hidden" />
             <img src={whiteLogo} alt="Logo" className="h-8 lg:h-12 hidden dark:block" />
           </a>
@@ -33,37 +33,37 @@ const NavBar = () => {
 
         {/* Middle Section: Nav Links for Larger Screens */}
         <div className="hidden lg:flex lg:space-x-8 flex-grow justify-center">
-          <a href="/" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium">Home</a>
-          <a href="/about" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium">About</a>
-          <a href="/projects" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium">Projects</a>
-          <a href="/contact" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium">Contact</a>
+          <a href="/" className="text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium">Home</a>
+          <a href="/about" className="text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium">About</a>
+          <a href="/projects" className="text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium">Projects</a>
+          <a href="/contact" className="text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium">Contact</a>
         </div>
 
         {/* Right Section: Button and Icons */}
         <div className="flex items-center space-x-4">
-          <a href="/quotes" className="hidden lg:block px-5 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full shadow hover:opacity-90 transition duration-300 dark:bg-blue-700 dark:hover:bg-blue-800 transition duration-300">
-            Leave a Quote
+          <a href="/quotes" className="font-medium hidden lg:block px-5 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full shadow hover:opacity-90 transition duration-300 dark:bg-blue-700 dark:hover:bg-blue-800 transition duration-300 hover:scale-105">
+            Leave a Quote 🥨
           </a>
-          <a href="https://github.com/PaulP1406" target="_blank" rel="noopener noreferrer" className="hidden lg:block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+          <a href="https://github.com/PaulP1406" target="_blank" rel="noopener noreferrer" className="hidden lg:block text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400">
             <FaGithub className="w-6 h-6" />
           </a>
           <a href="https://www.linkedin.com/in/ddpham/" target="_blank" rel="noopener noreferrer" className="hidden lg:block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
             <FaLinkedin className="w-6 h-6" />
           </a>
-          <button onClick={toggleTheme} className="hidden lg:block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 focus:outline-none">
+          <button onClick={toggleTheme} className="hidden lg:block text-gray-800 dark:text-gray-200 hover:text-yellow-500 dark:hover:text-yellow-400 focus:outline-none">
             {theme === 'dark' ? <FaSun className="w-6 h-6" /> : <FaMoon className="w-6 h-6" />}
           </button>
         </div>
 
         {/* Hamburger Menu and Icons */}
         <div className="flex items-center space-x-2 lg:hidden space-x-5">
-          <a href="https://github.com/PaulP1406" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+          <a href="https://github.com/PaulP1406" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-rose-500 dark:hover:text-rose-400">
             <FaGithub className="w-5 h-5" />
           </a>
           <a href="https://www.linkedin.com/in/ddpham/" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
             <FaLinkedin className="w-5 h-5" />
           </a>
-          <button onClick={toggleDarkMode} className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 focus:outline-none">
+          <button onClick={toggleDarkMode} className="text-gray-800 dark:text-gray-200 hover:text-yellow-500 dark:hover:text-yellow-400 focus:outline-none">
             {isDarkMode ? <FaSun className="w-5 h-5" /> : <FaMoon className="w-5 h-5" />}
           </button>
           <button onClick={toggleMenu} className="text-gray-800 dark:text-gray-200 focus:outline-none">
@@ -82,13 +82,13 @@ const NavBar = () => {
 
       {/* Mobile Menu */}
       <div className={`lg:hidden fixed inset-0 top-16 bg-white dark:bg-gray-800 bg-opacity-90 dark:bg-opacity-90 backdrop-blur-sm transition-all duration-300 ease-in-out transform ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'} flex flex-col items-start px-4 pt-2 pb-3 space-y-1 mg-left space-y-2.5`}>
-        <a href="/" className="block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium ml-12 text-lg">Home</a>
-        <a href="/about" className="block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium ml-12 text-lg">About</a>
-        <a href="/projects" className="block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium ml-12 text-lg">Projects</a>
-        <a href="/contact" className="block text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 font-medium ml-12 text-lg">Contact</a>
+        <a href="/" className="block text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium ml-12 text-lg">Home</a>
+        <a href="/about" className="block text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium ml-12 text-lg">About</a>
+        <a href="/projects" className="block text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium ml-12 text-lg">Projects</a>
+        <a href="/contact" className="block text-gray-800 dark:text-gray-200 hover:text-rose-600 dark:hover:text-rose-400 font-medium ml-12 text-lg">Contact</a>
         <div className="w-full flex justify-center">
-          <a href="/quotes" className="block px-5 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full shadow hover:opacity-90 transition duration-300 dark:bg-blue-700 dark:hover:bg-blue-800 transition duration-300">
-            Leave a Quote
+          <a href="/quotes" className="font-medium block px-5 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full shadow hover:opacity-90 transition duration-300 dark:bg-blue-700 dark:hover:bg-blue-800 transition duration-300">
+          Leave a Quote 🥨
           </a>
         </div>
       </div>
