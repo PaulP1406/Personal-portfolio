64778a252081418a8b41b89f341b27c30c86c0dd
PaulP1406 <ducduy0308@gmail.com>
2024-09-22 18:18:29 -0700

    Added new speedtracker for vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -13,19 +14,22 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Project />} />
-          <Route path="/quotes" element={<LeaveAQuote />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <SpeedInsights />
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/quotes" element={<LeaveAQuote />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
