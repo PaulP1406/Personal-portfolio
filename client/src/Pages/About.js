6211e19d6f00697fcd2668a7a9643e8ff8cf07c1
PaulP1406 <ducduy0308@gmail.com>
2024-09-23 19:34:37 -0700

    Fixed check out project link in about page

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -132,7 +132,7 @@ const About = () => {
           
         </div>
         <div className='pb-8 pt-12 text-center lg:text-right lg:mr-16'>
-                <a href="/contact" className="text-lg font-bold text-gray-600 dark:text-gray-400 group">
+                <a href="/projects" className="text-lg font-bold text-gray-600 dark:text-gray-400 group">
                         <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
                             Check out my projects
                         </span>
